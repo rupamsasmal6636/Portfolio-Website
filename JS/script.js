334c50e00888975bad4818d619233cd542691c14
Rupam Sasmal <rupam.sasmal.6636@gmail.com>
2022-05-28 12:31:42 +0530

    update on home page bio and download resume

--- JS/script.js
+++ JS/script.js
@@ -18,22 +18,3 @@ $(document).ready(function(){
 function submitalert(){
     alert("Submitted Successfully!");
 }
-$('#GetFile').on('click', function () {
-    $.ajax({
-        url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/172905/test.pdf',
-        method: 'GET',
-        xhrFields: {
-            responseType: 'blob'
-        },
-        success: function (data) {
-            var a = document.createElement('a');
-            var url = window.URL.createObjectURL(data);
-            a.href = url;
-            a.download = 'myfile.pdf';
-            document.body.append(a);
-            a.click();
-            a.remove();
-            window.URL.revokeObjectURL(url);
-        }
-    });
-});
